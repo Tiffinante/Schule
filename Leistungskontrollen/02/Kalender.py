3abaa83fb7da38aa4fe5c286bdb52a86f991a444
tiffinante <pskontomtk@web.de>
2021-12-06 17:53:37 +0100

    - in Kalender.py Tagberechnung Fertig

--- Leistungskontrollen/02/Kalender.py
+++ Leistungskontrollen/02/Kalender.py
@@ -2,6 +2,27 @@ import math
 
 months = ["Fehler", 'Januar', 'Februar', 'März', 'April', 'Mai', 'Juni', 'Juli', 'August', 'September', 'Oktober',
           'November', 'Dezember', ]
+wochentage = ["Sonntag", "Montag", "Dinstag", "Mittwoch", "Donnerstag", "Freitag", "Samstag", "Sonntag"]
+
+monats_codes = {
+    1: 6,
+    2: 2,
+    3: 2,
+    4: 5,
+    5: 0,
+    6: 3,
+    7: 5,
+    8: 1,
+    9: 4,
+    10: 6,
+    11: 2,
+    12: 4,
+}
+monats_s_codes = {# Codes für schaldjahre. Nur Januar und Februar
+    1: 5,
+    2: 2,
+
+}
 
 while True:
     start_jahr = input("Welches Jahr möchtest du sehen? :")
@@ -14,12 +35,15 @@ while True:
                 break
 
 print(months[monat])
-
-julianischer_monat = ["Fehler", 11, 12, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-m = julianischer_monat[monat]
-d = 1
-c = jahr // 100
-y = jahr % 100
-
-w = (d + (2.6 * m - 0.2) + y + (y / 4) + (c / 4) - 2 * c) % 7
-print(math.ceil(w)) # rundet nach oben floor rundet nach unten
+# Rechnung für ersten Tag im Monat
+# Rechnungs Quelle: https://www.youtube.com/watch?v=-EiX7a2SBeA
+jahres_code = ((jahr % 100 / 4) + jahr % 100) % 7
+# Abfrage nach einem schaldjahr
+if (jahr % 4 == 0 and jahr % 100 != 0 or jahr % 400 == 0) and monat < 3:
+    # Berechnung des Tages mit schaltjahr
+    tag = (1 + monats_s_codes.get(monat) + jahres_code) % 7
+else:
+    # Berechnung des Tages
+    tag = (1 + monats_codes.get(monat) + jahres_code) % 7
+# Abrunden der Zahl und Tag aus der Liste entnehmen
+print(wochentage[math.floor(tag)])
