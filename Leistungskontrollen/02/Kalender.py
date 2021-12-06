261bb54e2448802c7bee4bcfc178acd57cb0d97b
tiffinante <pskontomtk@web.de>
2021-12-06 22:07:57 +0100

    - in Kalender.py Formatirung des Kalenders angefangen

--- Leistungskontrollen/02/Kalender.py
+++ Leistungskontrollen/02/Kalender.py
@@ -18,9 +18,9 @@ monats_codes = {
     11: 2,
     12: 4,
 }
-monats_s_codes = {# Codes für schaldjahre. Nur Januar und Februar
+monats_s_codes = {  # Codes für schaldjahre. Nur Januar und Februar
     1: 5,
-    2: 2,
+    2: 1,
 
 }
 
@@ -35,6 +35,7 @@ while True:
                 break
 
 print(months[monat])
+
 # Rechnung für ersten Tag im Monat
 # Rechnungs Quelle: https://www.youtube.com/watch?v=-EiX7a2SBeA
 jahres_code = ((jahr % 100 / 4) + jahr % 100) % 7
@@ -46,4 +47,29 @@ else:
     # Berechnung des Tages
     tag = (1 + monats_codes.get(monat) + jahres_code) % 7
 # Abrunden der Zahl und Tag aus der Liste entnehmen
+print(tag)
 print(wochentage[math.floor(tag)])
+
+# Kalender
+print(" ")
+print(f"{months[monat]} - {jahr}")
+tageszeile = "|{:^4}|{:^4}|{:^4}|{:^4}|{:^4}|{:^4}|{:^4}|".format("Mo", "Di", "Mi", "Do", "Fr", "Sa", "So")
+print(tageszeile)
+# 30 oder 31 tage oder Februar
+if monat == 2:
+    if jahr % 4 == 0 and jahr % 100 != 0 or jahr % 400 == 0:
+        # 29 Tage
+        datezeile = "|{:^4}|{:^4}|{:^4}|{:^4}|{:^4}|{:^4}|{:^4}|".format("x", "x", "x", "x", "x", "x", "x")
+        print(datezeile)
+    else:
+        # 28 Tage
+        datezeile = "|{:^4}|{:^4}|{:^4}|{:^4}|{:^4}|{:^4}|{:^4}|".format("x", "x", "x", "x", "x", "x", "x")
+        print(datezeile)
+elif monat == 1 or 3 or 5 or 7 or 8 or 10 or 12:
+    # 31 Tage
+    datezeile = "|{:^4}|{:^4}|{:^4}|{:^4}|{:^4}|{:^4}|{:^4}|".format("x", "x", "x", "x", "x", "x", "x")
+    print(datezeile)
+else:
+    # 30 Tage
+    datezeile = "|{:^4}|{:^4}|{:^4}|{:^4}|{:^4}|{:^4}|{:^4}|".format("x", "x", "x", "x", "x", "x", "x")
+    print(datezeile)
