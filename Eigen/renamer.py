ed204c6dfc63d22facafeff3a88930ce69db1938
tiffinante <pskontomtk@web.de>
2021-12-26 22:03:43 +0100

    -

---
+++ Eigen/renamer.py
@@ -0,0 +1,24 @@
+import os, time
+
+'''
+Wenn eine Datei in einen ordner reingelegt wird,
+wird sie automatisch nummereiert 
+wenn alse 2 drinne sind heist sie 3
+'''
+
+safe_len = 0
+path = input('Path:')
+old = input("name to rename")
+
+while True:
+    os.chdir(path)
+    len_ordner = len(os.listdir('.'))
+    if safe_len == 0:
+        safe_len = len_ordner
+    if safe_len != len_ordner:
+        if safe_len > len_ordner:
+            print("Error")
+        else:
+            time.sleep(0.1)
+            safe_len = len_ordner
+            os.renames(old, str(len_ordner) + ".png")
