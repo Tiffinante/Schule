65ebff07d47a7ed63a7ee193f3db34d5e56b65e2
mika <PSKontoMTK@web.de>
2022-01-11 12:54:12 +0100

    -domino aufgabe von David geklaut

--- Einstieg/for-Schleifen2.py
+++ Einstieg/for-Schleifen2.py
@@ -49,5 +49,8 @@ while True:
 print('''4. Dominosteine''')
 for x in range(1, 7):
     for y in range(0, 7):
-        print(f"({x}/{y})", end=" ")
+        if x == 0 and y > 0 or x == 6 and y < 6 or x == 1 and y < 1 or x == 2 and y < 2 or x == 3 and y < 3 or x == 4 and y < 4 or x == 5 and y < 5:
+            print("", end=" ")
+        else:
+            print(f"({x}/{y})", end=" ")
     print("")
