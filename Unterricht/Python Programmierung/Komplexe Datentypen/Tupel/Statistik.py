8315a90ff92fa0a67749d709e5040d68c5011fac
mika <PSKontoMTK@web.de>
2022-02-08 10:52:21 +0100

    -

--- Unterricht/Python Programmierung/Komplexe Datentypen/Tupel/Statistik.py
+++ Unterricht/Python Programmierung/Komplexe Datentypen/Tupel/Statistik.py
@@ -1,6 +1,8 @@
 import random
 import math
 
+import numpy
+
 len_of_tuple = 20
 temp = []
 
@@ -46,19 +48,32 @@ for i in range(len_of_tuple - 1):
 print(mxdzn)
 
 print("\nMinimale Distanz zwischen Nachbarwerten:")
-mndzn = 2**128
+mndzn = 101
 for i in range(len_of_tuple - 1):
     diff = my_tuple_sort[i + 1] - my_tuple_sort[i]
     if diff < mxdzn:
         mndzn = diff
 print(mndzn)
 
-print("\n\nMxDzN -> Maximale Distanz zwischen Nachbarwerten")
-print("MnDzN -> Minimale Distanz zwischen Nachbarwerten\n")
+print("\nDie Quantile 90, 75, 25 und 10")
+q90 = my_tuple_sort[((90 * len_of_tuple) // 100) - 1]
+q75 = my_tuple_sort[((75 * len_of_tuple) // 100) - 1]
+q25 = my_tuple_sort[((25 * len_of_tuple) // 100) - 1]
+q10 = my_tuple_sort[((10 * len_of_tuple) // 100) - 1]
+print(q90, q75, q25, q10)
 
-header = "{:<8}  {:<12}  {:<8}  {:<20}  {:<7}  {:<7}".format("", "Mittelwert", "Median", "Standardabweichung", "MxDzN",
-                                                             "MnDzN", )
+print("\n\nMaxDzN -> Maximale Distanz zwischen Nachbarwerten")
+print("MinDzN -> Minimale Distanz zwischen Nachbarwerten\n")
+
+header = "{:<8}  {:<12}  {:<8}  {:<20}  {:<7}  {:<7}".format("", "Mittelwert", "Median", "Standardabweichung", "MaxDzN",
+                                                             "MinDzN", )
 text_my_tuple = "{:<8}  {:<12}  {:<8}  {:<20}  {:<7}  {:<7}".format("my_tuple", mittelwert, median, stdv, mxdzn,
                                                                     mndzn, )
 print(header)
 print(text_my_tuple)
+
+print("\nQuantile:")
+print("90 = ", q90)
+print("75 = ", q75)
+print("27 = ", q25)
+print("10 = ", q10)
