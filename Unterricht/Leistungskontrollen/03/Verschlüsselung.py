52ea0c24e34e97626bc069552caaba8b858ea374
mika <PSKontoMTK@web.de>
2022-02-22 13:24:59 +0100

    -

--- Unterricht/Leistungskontrollen/03/Verschlüsselung.py
+++ Unterricht/Leistungskontrollen/03/Verschlüsselung.py
@@ -1,5 +1,33 @@
 # Caesar
-alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-code = "LBHORGGREPURPXLBHEFRYSORSBERLBHGENPXLBHEFRYS"
-for i in code:
-    print(alpha[alpha.index(i) - 3], end="")
+alpha = "abcdefghijklmnopqrstuvwxyz".upper()
+
+
+while True:
+    print("Soll Ver- oder Entschlüsselt werden?\n1 Für Verschlüsseln 2 Für Entschlüsseln")
+    eingabe = input("1 oder 2 :")
+    if not eingabe:
+        quit()
+
+    if eingabe.isdecimal():
+        eingabe = int(eingabe)
+        if 0 < eingabe < 3:
+            if eingabe == 1:
+                verschleusseln = True
+                print("Es wird Verschlüsselt")
+                break
+            else:
+                verschleusseln = False
+                print("Es wird Entschlüsselt")
+                break
+
+while True:
+    code = input("Code:")
+    if code.isalpha():
+        if code.isupper():
+            break
+
+
+while True:
+    verschiebung = input("Um wie viele Stehlen soll Verschoben?\n:")
+    if verschiebung.isdecimal():
+        verschiebung = int(eingabe)
