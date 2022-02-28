e54245a01fdb80e016b7d1f2684bb35d89f4d09b
mika <PSKontoMTK@web.de>
2022-02-28 10:58:43 +0100

    -

--- Unterricht/Leistungskontrollen/03/Verschlüsselung.py
+++ Unterricht/Leistungskontrollen/03/Verschlüsselung.py
@@ -1,33 +1,54 @@
 # Caesar
 alpha = "abcdefghijklmnopqrstuvwxyz".upper()
 
-
 while True:
-    print("Soll Ver- oder Entschlüsselt werden?\n1 Für Verschlüsseln 2 Für Entschlüsseln")
-    eingabe = input("1 oder 2 :")
-    if not eingabe:
-        quit()
+    while True:
+        print("\nSoll Ver- oder Entschlüsselt werden?\n1 Für Verschlüsseln 2 Für Entschlüsseln")
+        eingabe = input("1 oder 2 :") # 1 = Verschlüsseln 2 = Entschlüssln
+        if not eingabe:
+            quit()
 
-    if eingabe.isdecimal():
-        eingabe = int(eingabe)
-        if 0 < eingabe < 3:
-            if eingabe == 1:
-                verschleusseln = True
-                print("Es wird Verschlüsselt")
+        if eingabe.isdecimal():
+            eingabe = int(eingabe)
+            if 0 < eingabe < 3:
+                if eingabe == 1:
+                    verschleusseln = True
+                    print("Es wird Verschlüsselt")
+                    break
+                else:
+                    verschleusseln = False
+                    print("Es wird Entschlüsselt")
+                    break
+
+    while True: # Code Eingeben
+        if not eingabe:
+            quit()
+
+        code = input("Code:")
+        if code.isalpha():
+            if code.isupper():
                 break
+
+    while True: # Um wie viele stellen das Verschoben werden
+        if not eingabe:
+            quit()
+
+        verschiebung = input("Um wie viele Stehlen soll Verschoben?\n:")
+        if verschiebung.isdecimal():
+            verschiebung = int(verschiebung)
+            if 1 < verschiebung < 26:
+                break
+
+    print()
+
+    if verschleusseln: # Verschlüsseln
+        for element in code:
+            if alpha.index(element) + verschiebung >= 26:
+                print(alpha[(alpha.index(element) + verschiebung) - 26], end="")
             else:
-                verschleusseln = False
-                print("Es wird Entschlüsselt")
-                break
+                print(alpha[alpha.index(element) + verschiebung], end="")
+    else: # Entschlüsseln
+        for element in code:
+            print(alpha[alpha.index(element) - verschiebung], end="")
 
-while True:
-    code = input("Code:")
-    if code.isalpha():
-        if code.isupper():
-            break
-
-
-while True:
-    verschiebung = input("Um wie viele Stehlen soll Verschoben?\n:")
-    if verschiebung.isdecimal():
-        verschiebung = int(eingabe)
+    print()
